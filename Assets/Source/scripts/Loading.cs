94492faf3274899dd17a6c9e06aeabcfc8a7250c
Taranis <solovyov.production@gmail.com>
2023-06-17 11:40:36 +0300

    Added Loading class

--- Assets/Source/scripts/Loading.cs
+++ Assets/Source/scripts/Loading.cs
@@ -1,8 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Loading : MonoBehaviour
 {
-     
+     [SerializeField] private Slider _slider;
+     [SerializeField] private TextMeshProUGUI _text;
+     private float _timeCount;
+     private float _fillValue;
+
+     public Action OnDone;
+     private Action _onFixedUpdate;
+
+     public void Start()
+     {
+          Run(2);
+     }
+
+     public void Run(float timeCount)
+     {
+          _slider.value = 0;
+          _text.text = "0%";
+          _timeCount = timeCount;
+          _fillValue = 1 / _timeCount * Time.fixedDeltaTime;
+          _onFixedUpdate += Progress;
+     }
+
+     private void Progress()
+     {   
+          _slider.value += _fillValue;
+          _text.text = Mathf.RoundToInt(_slider.value * 100) + "%";
+          if (_slider.value >= 1)
+          {
+               _onFixedUpdate -= Progress;
+               OnDone?.Invoke();
+          }
+     }
+
+     private void FixedUpdate()
+     {
+          _onFixedUpdate?.Invoke();
+     }
 }
